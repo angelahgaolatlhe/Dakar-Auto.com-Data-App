b9d3a504bd84c926a8e6e0b6280f8aff56bd40c6
Angelah Kgato Gaolatlhe <gaolathie.a.kgato@aims-senegal.org>
2025-12-09 17:51:48 +0000

    Delete forms directory

--- forms/google.py
+++
@@ -1,7 +0,0 @@
-import streamlit as st
-
-st.title("Google Form")
-
-url = "https://forms.gle/T7iejCxbwXw5Fg7K8"
-
-st.components.v1.iframe(url, height=600)
--- forms/kobo.py
+++
@@ -1,7 +0,0 @@
-import streamlit as st
-
-st.title("KoboToolbox Form")
-
-url = "https://ee.kobotoolbox.org/x/zZ3tUhbQ"
-
-st.components.v1.iframe(url, height=600)
